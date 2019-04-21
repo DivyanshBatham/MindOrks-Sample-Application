eda5072ceef231388a1a72628aa581de900e859d
Divyansh Batham <divyansh.sunita@gmail.com>
2019-04-22 04:59:22 +0530

    Updates

---
+++ precache-manifest.bf2946db0e05bccf9f97d8fc87bc549b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499b8209c80b6b277a1a",
+    "url": "/MindOrks-Sample-Application/static/js/runtime~main.dab5e643.js"
+  },
+  {
+    "revision": "1b0246c86efbc8122b7d",
+    "url": "/MindOrks-Sample-Application/static/js/main.57fc820d.chunk.js"
+  },
+  {
+    "revision": "8159ac7cc6ac528feb97",
+    "url": "/MindOrks-Sample-Application/static/js/2.a9b06a75.chunk.js"
+  },
+  {
+    "revision": "1b0246c86efbc8122b7d",
+    "url": "/MindOrks-Sample-Application/static/css/main.4a16e4e2.chunk.css"
+  },
+  {
+    "revision": "6f46a8c03c599c6b055c10035ceed874",
+    "url": "/MindOrks-Sample-Application/index.html"
+  }
+];
